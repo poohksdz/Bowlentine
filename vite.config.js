ddd28723b3c5ed874865a281a2f3deb35afa9a5a
poohksdz <poohksdz@gmail.com>
2026-02-14 01:18:25 +0700

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/valentines-day-proposal"
+  base: "/",   // 👈 ใส่บรรทัดนี้
 })
